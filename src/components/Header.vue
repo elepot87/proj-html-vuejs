66f91a5bdccd205c2459f92d0e2fc611c7592002
elepot87 <eleonorapotente@gmail.com>
2021-12-09 18:41:43 +0100

    aggiunta prima parte hero

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,39 @@
 <template>
-  <header class="d-flex">
-    <div class="left d-flex align-items-center">
-      <img
-        :src="require(`@/assets/images/logo-svg.svg`)"
-        alt="logo"
-        class="logo-header"
-      />
-    </div>
-    <div class="right d-flex justify-content-end align-items-center">
-      <ul class="main-menu d-flex">
-        <li class="menu-item"><a href="#">Home</a></li>
-        <li class="menu-item"><a href="#">Culinary History</a></li>
-        <li class="menu-item"><a href="#">Our Team</a></li>
-        <li class="menu-item"><a href="#">Our Menu</a></li>
-        <li class="menu-item"><a href="#">Takeout</a></li>
-        <li class="menu-item"><a href="#">Bulletin</a></li>
-        <li class="menu-item"><a href="#">Reservations</a></li>
-        <li class="menu-item">
-          <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </li>
-      </ul>
-    </div>
-  </header>
+  <div class="big-container">
+    <header class="d-flex">
+      <div class="left d-flex align-items-center">
+        <img
+          :src="require(`@/assets/images/logo-svg.svg`)"
+          alt="logo"
+          class="logo-header"
+        />
+      </div>
+      <div class="right d-flex justify-content-end align-items-center">
+        <ul class="main-menu d-flex">
+          <li class="menu-item"><a href="#">Home</a></li>
+          <li class="menu-item"><a href="#">Culinary History</a></li>
+          <li class="menu-item"><a href="#">Our Team</a></li>
+          <li class="menu-item"><a href="#">Our Menu</a></li>
+          <li class="menu-item"><a href="#">Takeout</a></li>
+          <li class="menu-item"><a href="#">Bulletin</a></li>
+          <li class="menu-item"><a href="#">Reservations</a></li>
+          <li class="menu-item">
+            <a href="#"><i class="fas fa-shopping-cart"></i></a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <section class="hero d-flex">
+      <div class="hero-left">prova</div>
+      <div class="hero-right">
+        <!-- <img
+          :src="require(`@/assets/images/slider52x.jpg`)"
+          alt="sfondo-hero"
+          class="img-hero"
+        /> -->
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>

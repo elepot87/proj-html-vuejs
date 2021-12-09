2cf320d3bf7fc8ed47b09261565ff04d56e06507
elepot87 <eleonorapotente@gmail.com>
2021-12-09 18:10:48 +0100

    struttura flex per header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,25 @@
 <template>
-  <header>
-    <div class="card" style="width: 18rem">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+  <header class="d-flex">
+    <div class="left d-flex align-items-center">
+      <img
+        :src="require(`@/assets/images/logo-svg.svg`)"
+        alt="logo"
+        class="logo-header"
+      />
+    </div>
+    <div class="right d-flex justify-content-end align-items-center">
+      <ul class="main-menu d-flex">
+        <li class="menu-item"><a href="#">Home</a></li>
+        <li class="menu-item"><a href="#">Culinary History</a></li>
+        <li class="menu-item"><a href="#">Our Team</a></li>
+        <li class="menu-item"><a href="#">Our Menu</a></li>
+        <li class="menu-item"><a href="#">Takeout</a></li>
+        <li class="menu-item"><a href="#">Bulletin</a></li>
+        <li class="menu-item"><a href="#">Reservations</a></li>
+        <li class="menu-item">
+          <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>

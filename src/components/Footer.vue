105e33727e0354f1eb5363b93d5a6ea06c2c41f0
elepot87 <eleonorapotente@gmail.com>
2021-12-10 16:06:29 +0100

    aggiunti altri elementi dl footer main

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,34 @@
 <template>
   <footer>
     <section class="footer-main">
-      <div class="text">ciao ciao</div>
+      <div class="info-footer d-flex justify-content-center align-items-center">
+        <img
+          :src="require(`@/assets/images/logo-svg.svg`)"
+          alt="logo"
+          class="logo-footer"
+        />
+        <p class="text-footer">
+          Quam eu proin sit massa condimentum. Volutpat non pulvinar aliquet
+          nunc. Quam eu proin sit massa condimentum.
+        </p>
+        <ul class="icon-list d-flex">
+          <li class="icon-item">
+            <a href="#"><i class="fab fa-facebook-f icon-footer"></i></a>
+          </li>
+          <li class="icon-item">
+            <a href="#"><i class="fab fa-twitter icon-footer"></i></a>
+          </li>
+          <li class="icon-item">
+            <a href="#"><i class="fab fa-youtube icon-footer"></i></a>
+          </li>
+          <li class="icon-item">
+            <a href="#"><i class="fab fa-instagram icon-footer"></i></a>
+          </li>
+          <li class="icon-item">
+            <a href="#"><i class="fab fa-linkedin-in icon-footer"></i></a>
+          </li>
+        </ul>
+      </div>
     </section>
     <section class="copyright text-center">
       © Copyright 2012 - 2021 | Avada Theme by ThemeFusion | All Rights Reserved

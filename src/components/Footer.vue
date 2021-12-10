a9d7ea302b784b26878ed77c686260f9812b409e
elepot87 <eleonorapotente@gmail.com>
2021-12-10 15:36:30 +0100

    footer background

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <section class="footer-main">
+      <div class="text">ciao ciao</div>
+    </section>
+    <section class="copyright text-center">
+      © Copyright 2012 - 2021 | Avada Theme by ThemeFusion | All Rights Reserved
+      | Powered by WordPress
+    </section>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/footer";
+</style>
